03e0a82f8a49f2cd1330bcc7a77702921d8e3baa
MatiCenturion <maticentul50@gmail.com>
2025-03-20 16:52:23 -0300

    Se agrego nuevo proyecto guiado de youtube AristiDev

--- ColumnApplication/app/src/main/java/com/example/columnapplication/MainActivity.kt
+++ ColumnApplication/app/src/main/java/com/example/columnapplication/MainActivity.kt
@@ -6,17 +6,22 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -44,10 +49,20 @@ fun app() {
     ) {
         item {
             Image(
-                modifier = Modifier.fillMaxWidth().height(400.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(400.dp),
                 painter = painterResource(id = R.drawable.imagivandep),
                 contentDescription = "Logo IvanDevs"
             )
+            Row(modifier = Modifier.padding(top = 8.dp)) {
+                Image(
+                    painter = painterResource(id = R.drawable.favorite),
+                    contentDescription = "Like"
+                )
+            }
+
+            Text(text = "1", color = Color.White, modifier = Modifier.padding(start = 4.dp))
             Text(
                 text = "Ivan Devs",
                 fontSize = 32.sp,
@@ -69,6 +84,34 @@ fun app() {
                 color = Color.White,
 //            modifier = Modifier.fillMaxSize()
             )
+            LazyRow(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+
+            ) {
+                item {
+                    Text(text = "Stack", color = Color.White)
+                    Text(text = "Java", color = Color.White)
+                    Text(text = " Kotlin", color = Color.White)
+                    Text(text = " Kotlin", color = Color.White)
+                    Text(text = " Kotlin", color = Color.White)
+                    Text(text = " Kotlin", color = Color.White)
+                    Text(text = " Kotlin", color = Color.White)
+                    Text(text = " Kotlin", color = Color.White)
+                    Text(text = " Kotlin", color = Color.White)
+                    Text(text = " Kotlin", color = Color.White)
+                    Text(text = " Kotlin", color = Color.White)
+                    Text(text = " Kotlin", color = Color.White)
+                    Text(text = " Kotlin", color = Color.White)
+                    Text(text = " Kotlin", color = Color.White)
+                    Text(text = " Kotlin", color = Color.White)
+                    Text(text = " Kotlin", color = Color.White)
+                    Text(text = " Kotlin", color = Color.White)
+                }
+
+            }
         }
     }
 }
